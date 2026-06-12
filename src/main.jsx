03821ed866600a49a3e41bf35739a928191db122
RedeMafiosa <jsuispasportugues@gmail.com>
2026-06-12 21:30:40 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+
+const App = () => <h1>FlashStream OK</h1>
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 )
