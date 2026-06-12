c7e5cc20af7c441252bce755b385ca3b710946da
RedeMafiosa <jsuispasportugues@gmail.com>
2026-06-12 18:40:05 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import { Toaster } from "@/components/ui/toaster"
 import { Toaster as SonnerToaster } from "sonner"
-import { QueryClientProvider } from '@tanstack/react-query'
-import { queryClientInstance } from '@/lib/query-client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
-import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
 
 import Login from '@/pages/Login';
@@ -100,7 +97,6 @@ const AuthenticatedApp = () => {
 function App() {
   return (
     <AuthProvider>
-      <QueryClientProvider client={queryClientInstance}>
         <Router>
           <AuthenticatedApp />
         </Router>
