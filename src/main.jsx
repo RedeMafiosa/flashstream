259142ce42f477dfb4516ba727ba37869da67758
RedeMafiosa <jsuispasportugues@gmail.com>
2026-06-12 21:52:06 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-const App = () => <h1>FlashStream OK</h1>
+import App from './App.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
