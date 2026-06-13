7a5ffd9202d4c0609240226ac5b27cb32ac5525d
RedeMafiosa <jsuispasportugues@gmail.com>
2026-06-13 19:29:52 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,11 +7,14 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-  logLevel: 'error',
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  base: '/',
+  build: {
+    outDir: 'dist',
   },
 })
